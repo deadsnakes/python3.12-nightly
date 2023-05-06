9cede77cfec1497f41f77ff8095f97eb3065a3f5
Anthony Sottile <asottile@umich.edu>
2023-05-06 13:21:39 -0400

    mark _pydatetime as guarded

--- debian/pymindeps.py
+++ debian/pymindeps.py
@@ -110,6 +110,7 @@ excluded_imports = {
     'collections': set(('cPickle', 'pickle', 'doctest')),
     'compileall': set(('concurrent', 'multiprocessing')),
     'copy': set(('reprlib',)),
+    'datetime': {'_pydatetime'},
     #'functools': set(('_dummy_thread',)),
     'hashlib': set(('logging', '_hashlib')),
     #'hashlib': set(('_hashlib', '_md5', '_sha', '_sha2',)),
